1609e2a68d818c26f94388430bc911e55a24b351
Sudipta Basak <basaks@gmail.com>
2017-06-08 13:32:51 +1000

    replaced event processing by system calls to reduce memory leak by antelope

--- bin/export/events/extract_events.py
+++ bin/export/events/extract_events.py
@@ -8,10 +8,12 @@ from __future__ import print_function
 import os
 import sys
 import csv
+import subprocess
 import logging
 from optparse import OptionParser
-from ga_event2qml import event_xml, setup_event2qml, version
+from ga_event2qml import version
 from obspy.core.event import read_events
+
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -37,11 +39,10 @@ usage = "\n\t\tevent2qml [-h] [-v] [-d] [-p pfname] [-s XSD_schema] " \
             "database [OUT_DIR] \n"
 
 
-def extract_event(db_path, ev_file):
+def _extract_events(db_path, ev_file):
     """
-    :param db_path: database location 
-    :param ev_file: events file name, csv file
-    
+    :param db_path: str, database location 
+    :param ev_file: str, events file name, csv file    
     """
     ev_file = ev_file if ev_file else DEFAULT_EVENT_FILE
     with ds.closing(ds.dbopen(db_path, 'r')) as db:
@@ -54,28 +55,35 @@ def extract_event(db_path, ev_file):
                                      EVENT_FIELDS])
 
 
-def extract_all_events(ev, qml, db_path, output_dir, sc3xml):
+def _extract_all_events(cmd, output_dir, ev_file):
+    """
+    We repeatedly call dbopen to prevent memory accumulation during the 
+    repeated calls to database while exporting events quakeml file.
+    :param cmd: str, command string 
+    :param output_dir: str, dir where quakeml files should be saved  
+    :param ev_file: str, event file location
+    :return: 
+    """
+    with open(ev_file, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        reader.next()
+        for i, r in enumerate(reader):
+            event_id = r[0]
+            log.info('==========Processing event #{} '.format(i + 1) +
+                     'with event id #{}================'.format(event_id))
+            event_qml = os.path.join(output_dir, str(event_id) + '.xml')
+            ev_cmd = cmd + event_id + ' -o {}'.format(event_qml)
+            subprocess.check_call(ev_cmd, shell=True)
+            log.info('==========Finished processing event #{} '.format(i + 1) +
+                     'with event id #{}================'.format(event_id))
 
-    with ds.closing(ds.dbopen(db_path, 'r')) as db:
-        with ds.freeing(db.process(['dbopen event', 'dbsort evid'])) as view:
-            for i, row in enumerate(view.iter_record()):
-                _process_event(i, row, ev, qml, output_dir)
 
+def _convert_to_sc3xml(output_dir, sc3xml):
+    # read in all the QuakeML files into a catalogue object
     catalog = read_events(pathname_or_url=os.path.join(output_dir, '*.xml'),
                           format='QUAKEML')
-
+    # now write out seiscomp3ML
     catalog.write(filename=sc3xml, format='SC3ML')
-
-
-def _process_event(i, row, ev, qml, output_dir):
-    log.info('Processing event #{}: '.format(i + 1) +
-             ' '.join([str(row.getv(x)[0]) for x in EVENT_FIELDS]))
-    event_id = row.getv(EVENT_FIELDS[0])[0]
-    event_qml = os.path.join(output_dir, str(event_id) + '.xml')
-    event_xml(event_id=event_id,
-              event=ev,
-              quakeml=qml,
-              output_file=event_qml)
 
 
 if __name__ == '__main__':
@@ -88,7 +96,7 @@ if __name__ == '__main__':
     parser.add_option("-s", action="store", dest="schema",
                       default='', help="XML Schema Definition to implement")
 
-    # Vebose output
+    # Verbose output
     parser.add_option("-v", action="store_true", dest="verbose",
                       default=False, help="run with verbose output")
 
@@ -122,8 +130,6 @@ if __name__ == '__main__':
     log.setLevel(level=loglevel)
     log.info('loglevel=%s' % loglevel)
 
-    ev, qml = setup_event2qml(options=options, database=args[0])
-
     DB_PATH = os.path.abspath(args[0])
     assert os.path.exists(DB_PATH), 'provide correct path to db dir.'
 
@@ -136,5 +142,14 @@ if __name__ == '__main__':
     else:
         parser.error("Specified output dir '{}' exists.\n"
                      "Remove output dir and try again.".format(outdir_arg))
-    extract_all_events(ev, qml, DB_PATH, output_dir=outdir,
-                       sc3xml=options.output_file)
+
+    ev_file = os.path.join(outdir, 'events.csv')
+    _extract_events(db_path=DB_PATH, ev_file=ev_file)
+
+    cmd = 'python ga_event2qml.py ' \
+          '-s {} ' \
+          '-d ' \
+          '{} '.format(options.schema, DB_PATH)
+
+    _extract_all_events(cmd, output_dir=outdir, ev_file=ev_file)
+    _convert_to_sc3xml(output_dir=outdir, sc3xml=options.output_file)
